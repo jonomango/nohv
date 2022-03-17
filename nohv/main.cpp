513b38685275a4ef2e3ce1e2eaaa21c6caa9e333
jono <26609800+jonomango@users.noreply.github.com>
2022-03-17 14:19:02 -0700

    cleaned up output a bit.

--- nohv/main.cpp
+++ nohv/main.cpp
@@ -21,19 +21,23 @@ NTSTATUS driver_entry(PDRIVER_OBJECT driver, PUNICODE_STRING) {
   auto const affinity = KeSetSystemAffinityThreadEx(1);
 
   // cr0.cpp
+  DbgPrint("Testing cr0.cpp:\n");
   EXEC_DETECTION(cr0_detected_1);
 
   // cr3.cpp
+  DbgPrint("Testing cr3.cpp:\n");
   EXEC_DETECTION(cr3_detected_1);
   EXEC_DETECTION(cr3_detected_2);
 
   // xsetbv.cpp
+  DbgPrint("Testing xsetbv.cpp:\n");
   EXEC_DETECTION(xsetbv_detected_1);
   EXEC_DETECTION(xsetbv_detected_2);
   EXEC_DETECTION(xsetbv_detected_3);
   EXEC_DETECTION(xsetbv_detected_4);
 
   // timing.cpp
+  DbgPrint("Testing timing.cpp:\n");
   EXEC_DETECTION(timing_detected_1);
   EXEC_DETECTION(timing_detected_2);
 
